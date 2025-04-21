2b3e4ad6eb9a85f7e0fa92858b6798021fe75275
Moussa-codeur <correamoussa469@gmail.com>
2025-04-21 13:19:21 +0200

    Confirmation du mot de passe, Validation de chaque champ, création d'un bouton envoyer et enfin centrer le titre contact

--- mon_projet/lib/Connexion.dart
+++ mon_projet/lib/Connexion.dart
@@ -6,9 +6,10 @@ class Connexion extends StatefulWidget {
   @override
   State<Connexion> createState() => _ConnexionState();
 }
-
-class _ConnexionState extends State<Connexion> {
+class _ConnexionState extends State<Connexion> { 
+  // Cette clé est utilisée pour identifier et gérer l'état d'un formulaire Flutter
   final _formKey = GlobalKey<FormState>();
+  // Gère le texte saisi dans le champ de mot de passe.
   final _passwordController = TextEditingController();
 
   String nom = '';
@@ -19,7 +20,8 @@ class _ConnexionState extends State<Connexion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Contact')),
+      appBar: AppBar(title: const Text('Contact'),
+      centerTitle: true,),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -33,29 +35,51 @@ class _ConnexionState extends State<Connexion> {
                   prefixIcon: Icon(Icons.person),
                   border: OutlineInputBorder(),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ce champ est obligatoire';
+                  }
+                  return null;
+                },
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(height: 16),
 
               // Champ Prenom
-               TextFormField(
+              TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Prenom',
                   prefixIcon: Icon(Icons.person_outline),
                   border: OutlineInputBorder(),
                 ),
+                validator:
+                    (value) =>
+                        value == null || value.isEmpty
+                            ? 'Entrer votre prénom'
+                            : null,
+                onSaved: (value) => prenom = value!,
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(height: 16),
 
               // Champ Email
-               TextFormField(
+              TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Email',
                   prefixIcon: Icon(Icons.email),
                   border: OutlineInputBorder(),
                 ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Entrer votre email';
+                  } else if (!value.contains('@')) {
+                    return 'Email invalide';
+                  }
+                  return null;
+                },
+                onSaved: (value) => email = value!,
               ),
               const SizedBox(height: 16),
-              
+
               // Champ Mot de passe
               TextFormField(
                 controller: _passwordController,
@@ -64,8 +88,48 @@ class _ConnexionState extends State<Connexion> {
                   prefixIcon: Icon(Icons.lock),
                   border: OutlineInputBorder(),
                 ),
+                obscureText: true,
+                validator: (value) =>
+                    value != null && value.length < 6
+                        ? 'Minimum 6 caractères'
+                        : null,
+                onSaved: (value) => password = value!,
               ),
               const SizedBox(height: 16),
+
+              // Champ confirmation de mot de passe
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Confirmation de mot de passe',
+                  prefixIcon: Icon(Icons.lock_outline),
+                  border: OutlineInputBorder(),
+                ),
+                 obscureText: true,
+                validator: (value) {
+                  if (value != _passwordController.text) {
+                    return 'Les mots de passe ne correspondent pas';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 50),
+
+              // Champ bouton
+              ElevatedButton.icon(
+                icon: const Icon(Icons.send),
+                label: const Text("Envoyer"),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Message envoyé !')),
+                    );
+
+                    _formKey.currentState!.reset();
+                    _passwordController.clear();
+                  }
+                },
+              ),
             ],
           ),
         ),
